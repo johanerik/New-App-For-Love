1f3f485d619eaada2453f375c2b7aa3c3e87515b
TomasAcosta180230 <al180230@alumnos.uacj.mx>
2023-04-14 06:25:26 -0600

    New function and new db in firebase

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { View, Text, StyleSheet, StatusBar, TouchableOpacity, Image ,Animated  } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
-import MainStack from '../faceapp/navigation/MainStack';
+import MainStack from './navigation/MainStack'
 
 //Backend de la app Y Front de la app
 const App = () => {
